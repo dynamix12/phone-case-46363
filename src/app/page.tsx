82ab3f1c7eed3f081b41199cd0432d30afb2eb42
dynamix12 <kalin97@ymail.com>
2025-09-16 01:34:51 +0300

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,10 @@ export default function Home() {
               </span>{" "}
               say
             </h2>
-            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+            <img
+              src="/snake-2.png"
+              className="w-32 mt-2 -ml-4 order-0 lg:order-2"
+            />
           </div>
 
           <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
